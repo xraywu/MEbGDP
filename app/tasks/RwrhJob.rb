8dcd000721e68ca145e73ad0de446cca807a11bd
xraywu <xrayxiaoli@gmail.com>
2012-11-04 10:00:25 -0500

    Modified for linux

--- app/tasks/RwrhJob.rb
+++ app/tasks/RwrhJob.rb
@@ -1,6 +1,5 @@
 class RwrhJob < Struct.new(:omim_id, :top_results, :lambda, :gamma, :eta, :network, :task_id)
   
-  
   def perform
     script_path = Rails.root.join('app', 'tasks', 'rwrh')
     task_dir = "task_temp/#{task_id}"
@@ -9,10 +8,18 @@ class RwrhJob < Struct.new(:omim_id, :top_results, :lambda, :gamma, :eta, :netwo
     
     network.each do |n|
       file_number = Dir.entries(filepath).length
-      process = ChildProcess.build("matlab","-sd","#{script_path}",
-                                   "-nojvm","-nosplash","-nodesktop","-minimize","-r",
-                                   "#{n}(#{omim_id},#{top_results},#{lambda},#{gamma},#{eta},'#{filepath}')")
+      
+      if RUBY_PLATFORM.downcase.include?("mingw32") || RUBY_PLATFORM.downcase.include?("win32")
+        process = ChildProcess.build("matlab","-sd","#{script_path}",
+                                     "-nojvm","-nosplash","-nodesktop","-minimize","-r",
+                                     "#{n}(#{omim_id},#{top_results},#{lambda},#{gamma},#{eta},'#{filepath}')")
+      elsif RUBY_PLATFORM.downcase.include?("linux")
+        process = ChildProcess.build("matlab","-nodesktop","-r",
+                                     "#{n}\(#{omim_id},#{top_results},#{lambda},#{gamma},#{eta},\'#{filepath}\'\)")
+      end
+      
       process.start
+      
       while Dir.entries(filepath).length == file_number do
         sleep 1
       end
